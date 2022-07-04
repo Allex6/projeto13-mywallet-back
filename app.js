e019f41b9c424121a68edd6b332fb89dd1c37d70
Alex Souza <alex.webdevelloper@gmail.com>
2022-07-04 10:17:44 -0300

    fix: db conn

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cors from 'cors';
-import { MongoClient, ObjectId } from 'mongodb';
+import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
 import authRoutes from './routers/authRoutes.js';
 import transactionsRoutes from './routers/transactionsRoutes.js';
 
@@ -12,12 +12,13 @@ const app = express();
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017';
 const DB_NAME = process.env.DB_NAME || 'my_wallet';
 
-console.log('MONGO URI ', MONGO_URI, DB_NAME);
+const client = new MongoClient(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-const mongoClient = new MongoClient(MONGO_URI);
 let db;
 
-mongoClient.connect(() => db = mongoClient.db(DB_NAME));
+client.connect(err => {
+  db = client.db(DB_NAME);
+});
 
 app.use(cors());
 app.use(express.json());
